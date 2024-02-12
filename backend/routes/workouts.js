74a7b2aec822fbc74a806c363997cef65613c9e9
Programlog <varunk3249@gmail.com>
2024-02-12 13:29:59 -0500

    vid3

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', () => {})
+router.get('/', (request, response) => {
+    response.json({mssg:"GET all workouts"})
+})
 
-module.exports = router
+router.get('/:id', (request, response) => {
+    response.json({mssg:"GET single workout"})
+})
+
+router.post('/', (request, response) => {
+    response.json({mssg: "POST a new workout"})
+})
+
+router.delete('/:id', (request, response) => {
+    response.json({mssg: "DELETE a new workout"})
+})
+
+router.patch('/:id', (request, response) => {
+    response.json({mssg: "UPDATE a new workout"})
+})
+
+module.exports = router 
--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ const workoutRoutes = require('./routes/workouts')
 // launch and store express app
 const app = express()
 
+// middleware
+app.use(express.json())
 app.use((request, response, next) => {
     console.log(request.path, request.method)
     next()
 })
 
 // get routes
-app.use(workoutRoutes)
+app.use("/api/workouts", workoutRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
